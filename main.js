eb582caf54a8e25400fae08417b938a815cec62b
atuls9 <atul.sooryawanshi@gmail.com>
2023-02-22 18:54:34 +0530

    add edit &delete buttons and DOM task 8 completed

---
+++ main.js
@@ -0,0 +1,51 @@
+//first stem --> check main.js file linked to Html
+
+var form = document.getElementById('addForm');
+var itemlist = document.getElementById('items');
+
+
+//form submit event
+form.addEventListener('submit', additem);
+
+//delete event
+itemlist.addEventListener('click', removeItem)
+
+//additem function
+function additem(e){
+    e.preventDefault();
+
+    console.log(1);
+    //get Input value 
+    var newItem = document.getElementById('item').value
+    
+    //create new li
+    var li = document.createElement('li')
+   //add className
+    li.className = ('list-group-item');
+    li.appendChild(document.createTextNode(newItem))
+    //create delete button
+    var deletebtn = document.createElement('button');
+    deletebtn.className = ('btn btn-danger btn-sm float-right delete')
+    deletebtn.appendChild(document.createTextNode('X'))
+    var editbtn = document.createElement('button');
+    editbtn.className = ('btn btn-success float-right');
+    editbtn.appendChild(document.createTextNode('Edit'))
+    //append button to li
+    li.appendChild(deletebtn)
+    li.appendChild(editbtn)
+    
+
+    // console.log(li)
+    //append li to list
+    itemlist.appendChild(li)
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete'));{
+        if(confirm('Are You Sure')){
+            var li = e.target.parentElement;
+            itemlist.removeChild(li);
+        }
+    }
+
+}
