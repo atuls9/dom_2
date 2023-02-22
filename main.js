0c0d794eb9f8cf278ebb78ebdd82c7096a5e61a5
atuls9 <atul.sooryawanshi@gmail.com>
2023-02-22 20:58:33 +0530

    DOM task 9 i.e. filter for search Items is done

--- main.js
+++ main.js
@@ -2,13 +2,18 @@
 
 var form = document.getElementById('addForm');
 var itemlist = document.getElementById('items');
+var filter = document.getElementById('filter')
+
 
 
 //form submit event
 form.addEventListener('submit', additem);
 
-//delete event
+//delete event-
 itemlist.addEventListener('click', removeItem)
+
+//add search Item
+filter.addEventListener('keyup', filterItems)
 
 //additem function
 function additem(e){
@@ -16,13 +21,13 @@ function additem(e){
 
     console.log(1);
     //get Input value 
-    var newItem = document.getElementById('item').value
-    
+    var newItem = document.getElementById('item1').value
+    var newItem2 = document.getElementById('item2').value
     //create new li
     var li = document.createElement('li')
    //add className
     li.className = ('list-group-item');
-    li.appendChild(document.createTextNode(newItem))
+    li.appendChild(document.createTextNode(`${newItem} ${newItem2}`))
     //create delete button
     var deletebtn = document.createElement('button');
     deletebtn.className = ('btn btn-danger btn-sm float-right delete')
@@ -48,4 +53,31 @@ function removeItem(e){
         }
     }
 
+}
+
+
+function filterItems(e){
+    // conver text to lowercase
+    var text = e.target.value.toLowerCase();
+    // console.log(text)
+
+    //get list by tagname - li
+    var items = itemlist.getElementsByTagName('li');
+    //conver the list to aray
+
+    Array.from(items).forEach(function(item){
+
+        var itemName= item.firstChild.textContent;
+        // console.log(itemName);
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+
+    });
+
+
+           
+    
 }
